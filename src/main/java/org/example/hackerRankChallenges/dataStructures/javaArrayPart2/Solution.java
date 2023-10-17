99bac1d8920767d3b734098da758d90ad82889b3
lipe <lipers20@hotmail.com>
2023-10-17 17:58:05 -0300

    Data Structures - Java ArrayList Part 2 - passing the tests

--- src/main/java/org/example/hackerRankChallenges/dataStructures/javaArrayPart2/Solution.java
+++ src/main/java/org/example/hackerRankChallenges/dataStructures/javaArrayPart2/Solution.java
@@ -15,49 +15,25 @@ public class Solution {
                 game[i] = scan.nextInt();
             }
 
-            System.out.println((canWin(leap, game, n)) ? "YES" : "NO");
+            System.out.println((canWin(leap, game)) ? "YES" : "NO");
         }
         scan.close();
     }
 
-    public static boolean canWin(int leap, int[] game, int n) {
+    public static boolean canWin(int leap, int[] game) {
+        return isSolvable(leap, game, 0);
+    }
 
-        int i = 0;
-        while (true) {
-            if (won(i, n)) {
-                return true;
-            }
-            try {
-                if (game[i + leap] == 0) {
-                    i += leap;
-                    continue;
-                } else if (game[i + 1] == 0) {
-                    i++;
-                    continue;
-                }
-                boolean gotBackAndJumped = false;
-                if (i > 0 && game[i - 1] == 0) {
-                    for (int j = 1; j < leap; j++) {
-                        if (i > 0 && game[i - 1] == 0) {
-                            i--;
-                            if (game[i + leap] == 0) {
-                                i += leap;
-                                gotBackAndJumped = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (!gotBackAndJumped)
-                    return false;
-            } catch (Exception e) {
-                return true;
-            }
+    private static boolean isSolvable(int leap, int[] game, int i) {
+        if (i >= game.length) {
+            return true;
         }
+        if (i < 0 || game[i] == 1) {
+            return false;
+        }
+        game[i] = 1;
+        return isSolvable(leap, game, i + leap) ||
+                isSolvable(leap, game, i + 1) ||
+                isSolvable(leap, game, i - 1);
     }
-
-    private static boolean won(int i, int n) {
-        return n - 1 == i;
-    }
-
 }
